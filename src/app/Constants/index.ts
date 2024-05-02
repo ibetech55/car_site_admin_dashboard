970f6a7f9898c617b77becdbbdf428fd294bfd56
pibewiro <pibewiro@yahoo.com>
2024-05-02 14:27:04 -0300

    fix repo name

--- src/app/Constants/index.ts
+++ src/app/Constants/index.ts
@@ -1,4 +1,3 @@
 export const CONSTANTS = {
     XLSX_FILE_EXT:'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    XLSX_FILE_EXT23kk:'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 };
