f6d24f8b35dfee16bd8125845e21b88c30f2f9c6
pibewiro <pibewiro@yahoo.com>
2024-05-02 14:15:31 -0300

    test

--- src/app/Constants/index.ts
+++ src/app/Constants/index.ts
@@ -1,4 +1,3 @@
 export const CONSTANTS = {
     XLSX_FILE_EXT:'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    IMAGE_EXT:'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 };
